cd0af68420f0b3681769dd3269afdf29c737bccb
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-04-19 08:31:58 +0000

    added kbc payment method

--- renderers.js
+++ renderers.js
@@ -1,9 +1,11 @@
 import IDeal from './src/method/IDeal';
 import Creditcards from './src/method/Creditcards';
 import PayPal from './src/method/PayPal';
+import Kbc from './src/method/Kbc';
 
 export default {
   buckaroo_magento2_ideal: IDeal,
   buckaroo_magento2_creditcards: Creditcards,
   buckaroo_magento2_paypal: PayPal,
+  buckaroo_magento2_kbc: Kbc,
 };
