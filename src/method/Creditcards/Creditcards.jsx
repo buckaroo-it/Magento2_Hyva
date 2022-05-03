c47c4809b9dfe5f8951f47a66463ebf56b6fbfe3
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-05-03 13:59:19 +0000

    moved `determineIssuer` function outside component

--- src/method/Creditcards/Creditcards.jsx
+++ src/method/Creditcards/Creditcards.jsx
@@ -19,6 +19,7 @@ import { ADDITIONAL_DATA_KEY } from '../../lib/helpers/AdditionalBuckarooDataKey
 import { validationSchema } from './Validators';
 import TextInput from '../../lib/helpers/components/TextInput';
 import SelectInput from '../../lib/helpers/components/SelectInput';
+import determineIssuer from './helpers/DetermineIssuer';
 
 function Creditcards({ method, selected, actions }) {
   const isSelected = method.code === selected.code;
@@ -64,28 +65,6 @@ function Creditcards({ method, selected, actions }) {
   const range = (size, startAt = 0) =>
     [...Array(size).keys()].map((i) => i + startAt);
 
-  const determineIssuer = (cardnumber) => {
-    const issuerIdentificationNumbers = {
-      amex: '^3[47][0-9]{13}$',
-      maestro: '^(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}$',
-      dankort: '^(5019|4571)[0-9]{12}$',
-      mastercard: '^(5[1-5]|2[2-7])[0-9]{14}$',
-      visaelectron:
-        '^(4026[0-9]{2}|417500|4508[0-9]{2}|4844[0-9]{2}|4913[0-9]{2}|4917[0-9]{2})[0-9]{10}$',
-      visa: '^4[0-9]{12}(?:[0-9]{3})?$',
-    };
-    const foundIssuers = [];
-    Object.keys(issuerIdentificationNumbers).forEach((issuer) => {
-      if (cardnumber.match(issuerIdentificationNumbers[issuer])) {
-        foundIssuers.push(issuer);
-      }
-    });
-    if (foundIssuers.length) {
-      console.log(foundIssuers);
-      return foundIssuers.pop();
-    }
-    return null;
-  };
   const formik = useFormik({
     initialValues: {
       cardholder: '',
