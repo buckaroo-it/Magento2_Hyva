8e48bee5e7d18919fb2fc0727f0d7a98acb9d0bf
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-03-01 10:17:07 +0000

    update logo functionality

--- src/lib/helpers/getLogo.js
+++ src/lib/helpers/getLogo.js
@@ -1,69 +1,37 @@
-import ideal from '../../../assets/Ideal.svg';
-import creditcards from '../../../assets/Creditcards.svg';
-import paypal from '../../../assets/Paypal.svg';
-import kbc from '../../../assets/Kbc.svg';
-import mrcash from '../../../assets/Bancontact.svg';
-import giropay from '../../../assets/Giropay.svg';
-import giftcards from '../../../assets/Giftcards.svg';
-import eps from '../../../assets/EPS.svg';
-import transfer from '../../../assets/sepa-credittransfer.svg';
-import sepadirectdebit from '../../../assets/sepa-directdebit.svg';
-import afterpay20 from '../../../assets/AfterPay.svg';
-import applepay from '../../../assets/Applepay.svg';
+import RootElement from '@hyva/react-checkout/utils/rootElement';
 
-import payconiq from '../../../assets/Payconiq.svg';
-import belfius from '../../../assets/Belfius.svg';
-import sofortbanking from '../../../assets/Sofort.svg';
-import alipay from '../../../assets/Alipay.svg';
-import wechatpay from '../../../assets/WeChatPay.svg';
-import trustly from '../../../assets/Trustly.svg';
-import rtp from '../../../assets/RequestToPay.svg';
-
-import klarna from '../../../assets/klarna.svg';
-import payperemail from '../../../assets/payperemail.svg';
-import capayablein3 from '../../../assets/in3.svg';
-import tinka from '../../../assets/tinka.svg';
-import przelewy24 from '../../../assets/przelewy24.svg';
-import billink from '../../../assets/billink.svg';
+export const logoPath = `${RootElement.getFilePath()}/Buckaroo_Magento2/images/svg`;
 
 const logos = {
-  ideal,
-  idealprocessing: ideal,
-  creditcards,
-  paypal,
-  kbc,
-  mrcash,
-  giropay,
-  giftcards,
-  eps,
-  transfer,
-  afterpay20,
-  afterpay2: afterpay20,
-  afterpay: afterpay20,
-  creditcard: creditcards,
-  applepay,
-  payconiq,
-  belfius,
-  sofortbanking,
-  alipay,
-  wechatpay,
-  trustly,
-  rtp,
-  klarna,
-  klarnain: klarna,
-  klarnakp: klarna,
-  sepadirectdebit,
-  payperemail,
-  capayablein3,
-  tinka,
-  p24: przelewy24,
-  billink,
+  afterpay2: 'afterpay',
+  afterpay20: 'afterpay',
+  capayablein3: 'in3',
+  capayablepostpay: 'in3',
+  creditcard: 'creditcards',
+  creditcards: 'creditcards',
+  giftcards: 'giftcards',
+  idealprocessing: 'ideal',
+  klarnain: 'klarna',
+  klarnakp: 'klarna',
+  mrcash: 'bancontact',
+  p24: 'przelewy24',
+  sepadirectdebit: 'sepa-directdebit',
+  sofortbanking: 'sofort',
+  emandate: 'emandate.png',
+  pospayment: 'pos.png',
+  transfer: 'sepa-credittransfer',
 };
 
 export default function getLogo(methodCode) {
-  const logoName = methodCode.replace('buckaroo_magento2_', '');
+  let logoName = methodCode.replace('buckaroo_magento2_', '');
+
   if (logoName in logos) {
-    return logos[logoName];
+    logoName = logos[logoName];
   }
-  return undefined;
+
+  if (logoName === 'voucher') {
+    return undefined;
+  }
+
+  return `${logoPath}/${logoName}.svg`;
 }
