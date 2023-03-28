17b4257429af4928cbd1842f9fd6d5d962df2b51
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-03-28 10:44:27 +0000

    fix giftcard validation message

--- src/lib/hooks/giftcard/modifier.js
+++ src/lib/hooks/giftcard/modifier.js
@@ -1,5 +1,10 @@
 import { get as _get } from 'lodash-es';
 
 export default function modifyPlaceOrder(result) {
+  const errors = _get(result, 'errors', undefined);
+  console.warn(errors);
+  if (errors !== undefined) {
+    return errors;
+  }
   return _get(result, 'data.buckarooProcessGiftcardTransaction', {});
 }
