87164c6d702bb8e6b6fa2b88b60346f6ba3fa933
Buckaroo <office@buckaroo.nl>
2023-08-18 11:05:50 +0000

    fix formatting

--- src/method/AfterpayOld/helpers.js
+++ src/method/AfterpayOld/helpers.js
@@ -138,7 +138,8 @@ export function prepareValidationSchema(cart, paymentMethod) {
   return YupObject({
     telephone: YupString().when('canShowPhone', {
       is: () => canShowPhone(cart),
-      then: () => YupString().required(requiredMessage).bkOnlyNumeric(onlyNumeric),
+      then: () =>
+        YupString().required(requiredMessage).bkOnlyNumeric(onlyNumeric),
       otherwise: () => YupString(),
     }),
     dob: YupString()
@@ -148,14 +149,16 @@ export function prepareValidationSchema(cart, paymentMethod) {
     businessType: YupString().oneOf(['b2b', 'b2c'], requiredMessage),
     coc: YupString().when('aaa', {
       is: () => isDigiAccept(paymentMethod),
-      then: () => YupString().required(requiredMessage).bkOnlyNumeric(onlyNumeric),
+      then: () =>
+        YupString().required(requiredMessage).bkOnlyNumeric(onlyNumeric),
       otherwise: () => YupString(),
     }),
     iban: YupString().when('businessType', {
       is: (fields) => isB2B(paymentMethod, fields),
-      then: () => YupString()
-        .required(requiredMessage)
-        .bkValidateBankNumber(__('Please enter a valid account number')),
+      then: () =>
+        YupString()
+          .required(requiredMessage)
+          .bkValidateBankNumber(__('Please enter a valid account number')),
       otherwise: () => YupString(),
     }),
     companyName: YupString().when('businessType', {
